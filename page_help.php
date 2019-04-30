5753929ae64926939a141806979027f3bed7729e
diahputr <46917636+diahputr@users.noreply.github.com>
2019-04-30 15:49:45 +0700

    Add files via upload

---
+++ page_help.php
@@ -0,0 +1,25 @@
+<div class="card-body">
+            <div class="card-body" style="height: 880px; background-color: white">
+                <p class="text-muted" style="font-size:40px ;  font-weight: bold">Help</p> </br>
+                <p class="text-muted" style="font-size:30px ;  font-weight: bold">Status of Current Issues</p> </br>
+                <ul>
+                    <li>
+                        <p style="margin-top: -30px"> Lending looks different but works the same! <a href="#" >Please see this page</a> for information on what we're changing and how to interact with it. We're still updating it so please contact us if you get stuck.</p>
+                    </li>
+                </ul>
+                <p class="text-muted" style="font-size:30px ;  font-weight: bold">FAQ Categories</p> </br>
+                <p style="margin-top: -30px"> Please review our <a href="#"> Frequently Asked Questions.</a> We try to keep them up to date. Here are direct links to the main FAQ categories.</p>
+                <p class="text-muted" style="font-size:22px "> <a href="#">Learn About Open Library</a></p> 
+                <p class="text-muted" style="font-size:22px "> <a href="#">Learn About DAISY/Print Disabled Books on Open Library</a></p> 
+                <p class="text-muted" style="font-size:22px "> <a href="#">How to Read Books on Open Library</a></p> 
+                <p class="text-muted" style="font-size:22px "> <a href="#">How to Borrow Books Through Open Library</a></p> 
+                <p class="text-muted" style="font-size:22px "> <a href="#">How to Edit Open Library</a></p> 
+                <p class="text-muted" style="font-size:22px "> <a href="#">About Using Open Library Data</a></p> 
+                <p class="text-muted" style="font-size:22px "> <a href="#">Troubleshooting Common Problems</a></p> 
+                <p class="text-muted" style="font-size:30px ;  font-weight: bold">Contact Us</p> 
+                <p style="margin-top: -10px"> There are a few different ways to keep in touch with us.</p>
+                <p style="margin-top: -10px"> 1. <a href="#"> Interact with us on Twitter.</a> </p>
+                <p style="margin-top: -10px"> 2. Join one of our mailing lists: <a href="#"> General discussion (archives), Librarianship, Tech (archives).</a> </p>
+                <p style="margin-top: -10px"> 3.Contact us using the <a href="#"> Contact Form </a> or click the Problem flag which is in the footer of every page.</p>
+            </div>
+</div>
---
+++ page_search.php
@@ -0,0 +1,49 @@
+    <div class="card-body" style="height: 800px; background-color: white">
+            <p class="text-muted" style="font-size:40px ;  font-weight: bold">Advanced Search</p> </br>
+            <div class="form" style="margin-top: -30px">
+
+                    <div>
+                            <p style="margin-top: 10px">Title</p>
+                            <input style="width:500px ; height: 30px"type="text" class="iniform" placeholder="">
+                        </div>
+                        <div> 
+                                <p style="margin-top: 10px">Author</p>
+                                <input style="width:500px ; height: 30px"type="text" class="iniform" placeholder="">
+                        </div>
+                        <div>
+                                <p style="margin-top: 10px">ISBN</p>
+                                <input style="width:500px ; height: 30px"type="text" class="iniform" placeholder="">
+                        </div>
+                        <div>
+                                <p style="margin-top: 10px">Subject</p>
+                                <input style="width:500px ; height: 30px"type="text" class="iniform" placeholder="">
+                        </div>
+                        
+                        <div>
+                                <p style="margin-top: 10px">Place</p>
+                                <input style="width:500px ; height: 30px"type="text" class="iniform" placeholder="">
+                        </div>
+                        <div>
+                                <p style="margin-top: 10px">Person</p>
+                                <input style="width:500px ; height: 30px"type="text" class="iniform" placeholder="">
+                        </div>
+                        
+                        <div>
+                                <p style="margin-top: 30px">Publisher</p>
+                                <input style="width:500px ; height: 30px"type="text" class="iniform" placeholder="">
+                        </div>
+                
+            </div>
+                        
+            <style>
+            .iniform{
+                border: 1px solid black;
+            }
+            </style>
+
+            <button type="btn" class="btn btn-danger" style="margin-top: 15px ; height: 50px ; width :80px "> Search </button>
+            <p style="margin-top: 10px"> <a href="#"> Full Text Search? </a> </p>
+
+
+
+            </div>
